67ab1c8aca58b214eeaf203a56c6d30a0b40bc4c
Charles Leighton <chleighton1@gmail.com>
2023-09-06 14:23:03 -0400

    finish lesson

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, current) => sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((res, current) => res * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = num;
+  if (num == 0 || num == 1) {
+    return 1
+  }
+  while (num > 1) {
+    num--;
+    res *= num;
+  }
+  return res;
+ 	
 };
 
 // Do not edit below this line
