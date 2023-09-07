5296b5ca8f1c4c8a67360c36c7f35e73f9004595
Charles Leighton <chleighton1@gmail.com>
2023-09-07 19:22:23 -0400

    complete challange

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) {
+        return "OOPS";
+    }
+    let fib = [0, 1];
+    if (x < 2) {
+        return fib[x];
+    }
+    else {
+        let y = fib.length -1;
+        while (y <= x) {
+            fib.push(fib[y] + fib[y-1]);
+            y = fib.length -1;
+        }
+        return fib[x];
+    }
 
 };
 
