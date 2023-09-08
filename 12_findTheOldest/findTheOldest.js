836c93f036004c3ad6dc6e709c27a501e793a22c
Charles Leighton <chleighton1@gmail.com>
2023-09-07 21:05:57 -0400

    complete challange

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    oldest = people.sort(function(a, b) {
+        date = new Date()
+        if(!a.yearOfDeath) {
+            a.yearOfDeath = date.getFullYear()
+        }
+        if(!b.yearOfDeath) {
+            b.yearOfDeath = date.getFullYear()
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return oldest[0];
 
 };
 
